ccfc689200a8ee015540d3124f0f828ea6b5478c
orichcasperkevin <orichcaspertheghost@gmail.com>
2022-05-13 20:04:05 +0300

    Init

---
+++ remove.py
@@ -0,0 +1,17 @@
+# example.
+import sys
+import os
+from rembg import remove
+from PIL import Image
+
+path = sys.argv[1]
+
+input_path = path
+basename = os.path.basename(input_path)
+new_name = basename.split('.')[0] + '_bg-removed.' + basename.split('.')[1]
+output_path = new_name
+
+input = Image.open(input_path)
+output = remove(input)
+output = output.convert('RGB')
+output.save(output_path)
